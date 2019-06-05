4324875fd1b8e6a737424314307b18566d3b7fdf
wenjiax <495743376@qq.com>
2019-06-05 13:37:43 +0800

    新增中序遍历迭代实现
    新增中序遍历迭代实现及测试代码。

--- binarytree/inorder_test.go
+++ binarytree/inorder_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/wenjiax/go-algorithms/binarytree"
 )
 
-func TestPreOrder(t *testing.T) {
+func TestInOrder(t *testing.T) {
 	tree := binarytree.NewBinarySearchTree()
 	//
 	//                         33
@@ -38,52 +38,49 @@ func TestPreOrder(t *testing.T) {
 	tree.Insert(66)
 	tree.Insert(55)
 
-	// 前序遍历
+	// 中序遍历
 
-	// [33 16 13 15 18 17 25 19 27 50 34 58 51 55 66]
-	t.Log(preOrder(tree.Find(33)))
+	// [13 15 16 17 18 19 25 27 33 34 50 51 55 58 66]
+	t.Log(inOrder(tree.Find(33)))
 
-	// [33 16 13 15 18 17 25 19 27 50 34 58 51 55 66]
-	t.Log(preOrder1(tree.Find(33)))
+	// [13 15 16 17 18 19 25 27 33 34 50 51 55 58 66]
+	t.Log(inOrder1(tree.Find(33)))
 }
 
-// 前序遍历
-func preOrder(root *binarytree.TreeNode) []int {
+// 中序遍历, 递归实现
+func inOrder(root *binarytree.TreeNode) []int {
 	if root == nil {
 		return nil
 	}
 
-	return append(append(append([]int{}, root.Val()), preOrder(root.Left())...), preOrder(root.Right())...)
+	return append(append(append([]int{}, inOrder(root.Left())...), root.Val()), inOrder(root.Right())...)
 }
 
-// 前序遍历非递归实现
-func preOrder1(root *binarytree.TreeNode) []int {
-	var result []int
-
-	// 利用栈先进后出特性
+// 中序遍历, 非递归实现
+func inOrder1(root *binarytree.TreeNode) []int {
 	var stack []*binarytree.TreeNode
+	var result []int
 
 	p := root
 	for p != nil || len(stack) > 0 {
 
 		// 访问所有左子节点
 		for p != nil {
-			result = append(result, p.Val())
-			// 加入栈
+			// 入栈
 			stack = append(stack, p)
 			p = p.Left()
 		}
 
-		// 所有左子节点访问完, 访问节点右子节点
+		// 左子节点访问完, 访问右子节点
 		if len(stack) > 0 {
-			// 获取栈元素
+			// 出栈
 			n := len(stack) - 1
 			p = stack[n]
 			stack = stack[:n]
 
+			result = append(result, p.Val())
 			p = p.Right()
 		}
-
 	}
 
 	return result
