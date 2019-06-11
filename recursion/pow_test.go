5ae54c1fd5b8514024690fa2029d5a6ef90ccc79
wenjiax <495743376@qq.com>
2019-06-11 17:06:34 +0800

    新增Pow 递归实现
    新增Pow 递归实现及测试代码。

--- recursion/pow_test.go
+++ recursion/pow_test.go
@@ -12,17 +12,40 @@ func TestMyPow(t *testing.T) {
 	// 输入: 2.00000, 10
 	// 输出: 1024.00000
 	t.Log(myPow1(2.0, 10))
+	t.Log(myPow(2.0, 10))
 
 	// 示例 2:
 	// 输入: 2.10000, 3
 	// 输出: 9.26100
 	t.Log(myPow1(2.1, 3))
+	t.Log(myPow(2.1, 3))
 
 	// 示例 3:
 	// 输入: 2.00000, -2
 	// 输出: 0.25000
 	// 解释: 2-2 = 1/22 = 1/4 = 0.25
 	t.Log(myPow1(2.0, -2))
+	t.Log(myPow(2.0, -2))
+}
+
+// 计算 x 的 n 次幂, 递归实现
+func myPow(x float64, n int) float64 {
+	// x 的 0 次幂
+	if n == 0 {
+		return 1
+	}
+
+	// x 的 -n 次幂
+	if n < 0 {
+		return 1 / myPow(x, -n)
+	}
+
+	// n 为奇数
+	if n%2 != 0 {
+		return x * myPow(x, n-1)
+	}
+
+	return myPow(x*x, n/2)
 }
 
 // 计算 x 的 n 次幂, 非递归实现
