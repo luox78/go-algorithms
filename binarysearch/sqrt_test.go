6ee0428208fbfc3a31c17cb3838aaf7a35259495
wenjiax <495743376@qq.com>
2019-06-19 23:11:31 +0800

    Update sqrt_test.go

--- binarysearch/sqrt_test.go
+++ binarysearch/sqrt_test.go
@@ -19,17 +19,23 @@ func TestSqrt(t *testing.T) {
 	// 说明: 8 的平方根是 2.82842...,
 	// 	 由于返回类型是整数，小数部分将被舍去。
 	t.Log(sqrt(8))
+
+	t.Logf("%f", 10e-9)
 }
 
 func sqrt(x int) int {
+	if x == 0 || x == 1 {
+		return x
+	}
+
 	i, j := 1, x
 
 	for i <= j {
 		mid := i + (j-i)>>1
 
-		if mid*mid > x {
+		if mid > x/mid {
 			j = mid - 1
-		} else if mid*mid < x {
+		} else if mid < x/mid {
 			i = mid + 1
 		} else {
 			return mid
